3d7df7e9f2e0a9e3f1e811681538cccf80edbedb
DongH-e-e <163309335+DongH-e-e@users.noreply.github.com>
2024-05-22 16:45:48 +0900

    Add files via upload

---
+++ 10주차 과제/Decode .v
@@ -0,0 +1,14 @@
+module Decode (
+    input wire [15:0] instruction,
+    output reg [3:0] opcode,
+    output reg [3:0] operand1,
+    output reg [3:0] operand2
+);
+
+always @(*) begin
+    opcode = instruction[15:12];
+    operand1 = instruction[11:8];
+    operand2 = instruction[7:4];
+end
+
+endmodule
